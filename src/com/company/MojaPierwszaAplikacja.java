8f149c92f02b532fd28d0299c2909a5ae8903f0b
Piotr Piekielko <piotrpiekielko17@gmail.com>
2018-07-01 13:03:22 +0200

    Initial commit

---
+++ src/com/company/MojaPierwszaAplikacja.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class MojaPierwszaAplikacja{
+
+    public static void main(String[] args) {
+	System.out.println("Ja rządze!");
+	System.out.println("Światem");
+    }
+}
